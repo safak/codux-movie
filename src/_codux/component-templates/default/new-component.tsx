95579c812c4c64bde96a72178ed534b1b2605a80
Safak <safakkocaoglu@hotmail.com>
2023-03-01 17:16:15 +0100

    Initial commit

---
+++ src/_codux/component-templates/default/new-component.tsx
@@ -0,0 +1,14 @@
+import styles from './new-component.module.scss';
+import classNames from 'classnames';
+
+export interface NewComponentProps {
+    className?: string;
+}
+
+/**
+ * This component was created using Codux's Default new component template.
+ * To create custom component templates, see https://help.codux.com/kb/en/article/configuration-for-new-components-and-templates
+ */
+export const NewComponent = ({ className }: NewComponentProps) => {
+    return <div className={classNames(styles.root, className)}>NewComponent</div>;
+};
